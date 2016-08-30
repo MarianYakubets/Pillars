3f1e955f2656bb7c2ecff254ae1d71b13c36efdd
Marian Iakubets <marian.iakubets@perfectial.com>
2016-08-30 16:23:21 +0300

    s

--- src/Game.js
+++ src/Game.js
@@ -1,12 +1,14 @@
 BasicGame.Game = function(game) {
 
-    //	When a State is added to Phaser it automatically has the following properties set on it, even if they already exist:
+    //	When a State is added to Phaser it automatically has the following properties set on it, 
+    //  even if they already exist:
 
     this.game; //	a reference to the currently running game
     this.add; //	used to add sprites, text, groups, etc
     this.camera; //	a reference to the game camera
     this.cache; //	the game cache
-    this.input; //	the global input manager (you can access this.input.keyboard, this.input.mouse, as well from it)
+    this.input; //	the global input manager (you can access this.input.keyboard,
+    // this.input.mouse, as well from it)
     this.load; //	for preloading assets
     this.math; //	lots of useful common math operations
     this.sound; //	the sound manager - add a sound, play one, set-up markers, etc
@@ -19,7 +21,9 @@ BasicGame.Game = function(game) {
     this.rnd; //	the repeatable random number generator
 
     //	You can use any of these from any function within this State.
-    //	But do consider them as being 'reserved words', i.e. don't create a property for your own game called "world" or you'll over-write the world reference.
+    //	But do consider them as being 'reserved words', i.e. 
+    //  don't create a property for your own game called "world" or you'll over-write 
+    //  the world reference.
 
 };
 
@@ -75,7 +79,8 @@ BasicGame.Game.prototype = {
 
     update: function() {
 
-        //	Honestly, just about anything could go here. It's YOUR game after all. Eat your heart out!
+        //	Honestly, just about anything could go here. It's YOUR game after all. 
+        //  Eat your heart out!
 
     },
 
